8bc923aee7b724056acb879dd81242d21937918a
nabm <nathabmmb@gmail.com>
2021-08-23 23:41:13 -0300

    general refactoring

--- libro.py
+++ libro.py
@@ -5,19 +5,13 @@ librería estandar con 30 (digo yo, charlable) libros para la parte 1.1 de main.
 import random
 
 
-class Libro():
+class Libro:
     def __init__(self, isbn, titulo, genero, idioma, precio):
         self.isbn = isbn
         self.titulo = titulo
         self.genero = genero
         self.idioma = idioma
         self.precio = precio
-
-posibles_titulos = ["Mi planta de mandarina-limón",
-                    "Ratero y yo",
-]
-posibles_generos = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-posibles_idiomas = [1, 2, 3, 4, 5]
 
 
 def validar_isbn(posible_isbn):
@@ -47,10 +41,10 @@ def generar_isbn():
     cifras = []
     total = 0
     # genera las primeras 8 cifras
-    for i in range(8):
+    for c in range(8):
         cifra = random.randrange(10)
         cifras.append(cifra)
-        total += (10-i) * cifra
+        total += (10-c) * cifra
 
     # genera la cifra 9
     resto = total % 11
@@ -58,7 +52,6 @@ def generar_isbn():
     cifra_9 = (resto + estocastico) // 2
     cifras.append(cifra_9)
     total += 2*cifra_9
-    print("cifra 9:", cifra_9)
 
     # genera la cifra 10
     resto = total % 11
@@ -69,13 +62,10 @@ def generar_isbn():
         cifra_10 = 9 - resto
     else:
         cifra_10 = 11 - resto
-
-    print("cifra 10:", cifra_10)
     cifras.append(cifra_10)
 
-
     len_grupos = [1, 1, 1, 1]
-    for i in range(6):
+    for s in range(6):
         len_grupos[random.randrange(4)] += 1
 
     isbn = ""
@@ -85,40 +75,42 @@ def generar_isbn():
             isbn += str(cifras[contador])
             contador += 1
         isbn += "-"
-    return(isbn[:-1])
+    return isbn[:-1]
 
 
-def autoLib(n=30):
+def auto_fill(n=30):
+    posibles_generos = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    posibles_idiomas = [1, 2, 3, 4, 5]
+    posibles_titulos = ["Mi planta de mandarina-limón",
+                        "Ratero y yo",
+                        ]
+
     catalogo = []
-    for i in range(n):
+    for c in range(n):
         isbn = generar_isbn()
         titulo = random.choice(posibles_titulos)
         genero = random.choice(posibles_generos)
         idioma = random.choice(posibles_idiomas)
         precio = random.randrange(300) + round(random.random(), 2)
         libro = Libro(isbn, titulo, genero, idioma, precio)
+        catalogo.append(libro)
+    return catalogo
 
 
+if __name__ == "__main__":
+    # test para validar_isbn
+    result1 = validar_isbn("84-8181-227-7")
+    result2 = validar_isbn("74-8181-227-7")
+    result3 = validar_isbn("94-8181-227-8")
+    if (result1, result2, result3) != (True, False, True):
+        print("Error: validar_isbn")
 
-# Test para validar_isbn
-result1 = validar_isbn("84-8181-227-7")
-print(result1)
-result2 = validar_isbn("74-8181-227-7")
-print(result2)
-result3 = validar_isbn("94-8181-227-8")
-print(result3)
-
-#test para generar isbn
-print("\n"+"="*60+"\n")
-for i in range(10):
-    isbn = generar_isbn()
-    msj = "intento {} de 10: \n".format(i+1)
-    msj += "ISBN {}, resultado {}\n".format(isbn, validar_isbn(isbn))
-    print(msj)
-
-
-"""
-resto = 2*n + m
-n = resto // 2 + estocastico
-
-"""
+    # test para generar_isbn
+    print("\n"+"="*20+"TESTS"+"="*20+"\n")
+    errores = 0
+    for i in range(10000):
+        isbn = generar_isbn()
+        if not validar_isbn(isbn):
+            print("Error: generar_isbn ||", isbn)
+            errores += 1
+    print("{}% de errores...".format(errores/100))
